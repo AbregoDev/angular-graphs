8d7c60446fb985ae34ccc07fe8c46344915dc905
Bryan Moreno <abre.go@outlook.com>
2022-06-11 15:22:34 -0500

    Donnut chart

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -6,11 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  // Doughnut
+  doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'other'];
+  
+  doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [50, 20, 10, 20],
+        backgroundColor: ['#027373', '#038C7F', '#A9D9D0', '#F2E7DC'],
+        hoverBackgroundColor: ['#128383', '#139C8F', '#B9E9E0', '#FFF7EC'],
+        hoverBorderColor: ['#006363', '#007C6F', '#99C9C0', '#E2D7CC'],
+      },
+    ]
+  };
+  
+  doughnutChartType: ChartType = 'doughnut';
 }
