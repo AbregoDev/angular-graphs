cde99847ec6ef1c685c70edfe5d91071c566827d
Bryan Moreno <abre.go@outlook.com>
2022-06-11 13:42:45 -0500

    charts library menu barras component graficas barra component graficas dobles component

--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-barras',
@@ -8,9 +10,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarrasComponent implements OnInit {
 
+  @ViewChild(BaseChartDirective)
+  chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10,
+        max: 100,
+      }
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+    }
+  };
+  public barChartType: ChartType = 'bar';
+
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#BF545D', hoverBackgroundColor: '#CF646D' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#609FA6', hoverBackgroundColor: '#70AFB6' },
+      { data: [25, 32, 49, 88, 36, 23, 40], label: 'Series C', backgroundColor: '#145946', hoverBackgroundColor: '#246956' },
+    ]
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public randomize(): void {
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+    ];
+
+    this.barChartData.datasets[1].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+    ];
+
+    this.barChartData.datasets[2].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+    ];
+
+    this.chart?.update();
+  }  
 
 }
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  ruta: string;
+  texto: string;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
   ]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  menu: MenuItem[] = [
+    { ruta: '/graficas/barra', texto: 'Barras' },
+    { ruta: '/graficas/barra-doble', texto: 'Barras dobles' },
+    { ruta: '/graficas/dona', texto: 'Dona' },
+    { ruta: '/graficas/dona-http', texto: 'Dona Http' },
+  ];
 
 }
