2a063ef9bb0cfe712aa93b2d178a15bad7ae79cc
Bryan Moreno <abre.go@outlook.com>
2022-06-12 21:15:50 -0500

    Show data from http response

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
@@ -9,12 +10,30 @@ import { GraficasService } from '../../services/graficas.service';
 })
 export class DonaHttpComponent implements OnInit {
 
+  // Doughnut
+  doughnutChartData: ChartData<'doughnut'> = {
+    datasets: []
+  };
+  
+  doughnutChartType: ChartType = 'doughnut';
+
   constructor(private socialNetService: GraficasService) { }
 
   ngOnInit(): void {
     this.socialNetService.getSocialNetData()
       .subscribe((data) => {
-        console.log(data);
+        const labels: string[] = Object.keys(data);
+        this.doughnutChartData.labels = labels;
+
+        const values = Object.values(data);
+        this.doughnutChartData.datasets = [
+          {
+            data: values,
+            backgroundColor: ['#027373', '#038C7F', '#A9D9D0', '#F2E7DC', '#A9F0B0'],
+            hoverBackgroundColor: ['#128383', '#139C8F', '#B9E9E0', '#FFF7EC', '#B9FFC0'],
+            hoverBorderColor: ['#006363', '#007C6F', '#99C9C0', '#E2D7CC', '#A9EFB0'],
+          }
+        ]
       })
   }
 
